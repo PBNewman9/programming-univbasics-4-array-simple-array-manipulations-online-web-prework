3076f219d51a9e13806bb5afbef216a67bbb65a4
Phil Newman <pnewman@viimed.com>
2019-10-28 05:54:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,7 @@ end
 
 
 def using_insert(list_of_programming_languages, another_language)
- new_array=list_of_programming_languages.insert[4](another_language)
+ list_of_programming_languages.insert[4](another_language)
 end
 
 
